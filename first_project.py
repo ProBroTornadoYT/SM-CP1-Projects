d3d5446cda2329790d932b9092bbc4c77411212c
ProBroTornadoYT <shreenayan.mehta@ucas-edu.net>
2026-08-18 09:11:51 -0600

    first commit

---
+++ first_project.py
@@ -0,0 +1,5 @@
+#Shreenayan Mehta, First project!
+
+print("hello World this is shree")
+print("issac loves 67")
+print("I love pizza ")
---
+++ inputsOutputs.py
@@ -0,0 +1,17 @@
+# comments
+
+print("good morni'n") #output
+
+#octothorp lol this is octothorp- # <-more like tic tac toe XD
+
+print(" this is sum programimg 1 thingie")
+print("AGHHAHAHGAGAHHA we usal are exprirncing the tecnoacl isue")
+
+# inputing
+name= input("What is your name?: ")
+
+print("You are so smart welcome to class " + name)
+
+food= input("what your food likeing")
+
+print("I JUST LOVE "+ food +" So good and tasty like heavnly delight oh and dont get me started on the wonderful textures oohh") 

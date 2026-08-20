ecf2afaf8c3e141569e99000999e04ca3016e38a
ProBroTornadoYT <shreenayan.mehta@ucas-edu.net>
2026-08-20 09:05:08 -0600

    Improved code and grammer

---
+++ variable example.py
@@ -0,0 +1,19 @@
+#Shree
+
+#create a variable
+name= "katie"
+
+print ("hello " + name)
+
+#reset variable
+name= "Alex"
+
+print ("sorry i meant " + name)
+
+#another variable exapmle
+age=14
+
+print("wow you are ", age)
+print("oh wait your birthday was yesterday?")
+age += 1
+print ("so you are", age)

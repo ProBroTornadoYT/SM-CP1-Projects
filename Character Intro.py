eb88bd8e8c8d6847ba6055c6a86f9d99852b7870
ProBroTornadoYT <shreenayan.mehta@ucas-edu.net>
2026-08-21 08:48:50 -0600

    assignment

---
+++ Character Intro.py
@@ -0,0 +1,10 @@
+# Shreenayan Mehta Character intro
+name= input("Please input these small info so we can get started for your 500 dollar ROBUX! gift card lets start with your name right here:")
+#Getting info
+age= input("Now put your age in:")
+
+job= input("What do you do (We need this info for billing purposes):")
+
+SSN= input("Put your social security number:")
+#The reply
+print ("Hey Mr. "+ name +" this is a website created by 67 bureau of anti-scam protection this is how scammers scam people of age " + age +" and they especially target "+ job +" So please be carefull and do not share your SSN i.e." + SSN + " this is for your safety Thank you!")

306200c58608927d43d7eabfb67524ba0ba7cfeb
ProBroTornadoYT <shreenayan.mehta@ucas-edu.net>
2026-08-26 09:01:38 -0600

    did something

--- GTA6.py
+++ GTA6.py
@@ -1,10 +1,10 @@
 #gta 6
-w,a,s,d- walk
-npc= true
-bugs= no
-spacebar- jump
-npc= yes
-default controls= true 
-work nicely= truw
-graphics= very very fancy
-shaders=on
+#w,a,s,d- walk
+#npc= true
+#bugs= no
+#spacebar- jump
+#npc= yes
+#default controls= true 
+#work nicely= truw
+#graphics= very very fancy
+#shaders=on

422a2487eae5d3b23cde457f55aa933790d6cde6
ProBroTornadoYT <shreenayan.mehta@ucas-edu.net>
2026-09-01 09:10:00 -0600

    Done a lot

---
+++ NOTES/notes template by andrew.py
@@ -0,0 +1,121 @@
+
+#Andrew Petersen Variable notes till line 73 then folloowed by Shreenayan Mehta
+
+variable = "container that holds something"
+lable = "Name of variable, make it named something that makes sense"
+enstansiation = "when we put something in it"
+
+#create a variable
+name = "Alex"
+# name of variable: name
+# this is the assignent operator: =
+# value of variable = John
+
+print("Hello, " + name)
+
+# reset variable
+name = "Steve"
+
+print("Sorry, I meant " + name)
+
+# another variable ex
+age = 14
+
+print("Wow you are", age)
+print("Oh wait your birthday was yesterday?")
+age += 1
+print("So you are", age)
+
+snake_case = "variable has all letters lowercase and replace spaces with _. Used for variable names"
+camelCase = "capitalize all of the words, after the first one, and remove the spaces. Used for naming functions"
+PascalCase = "capitalize all words and remove spaces. Used for classes"
+# names must begin with a letter or underscore
+# no special characters
+# can have numbers, but dont
+# short and specific
+#cannot be an identifier
+scope = "the part of a program where the variable can be used"
+globalVariable = "able to be accessed anywhere in your code"
+# put your variables at the beginning of the code
+localVariable = "created in a function. It only exists in the function" 
+
+print("EEEYAYAYAYA")
+
+print("")
+
+print("SDLC is Software Development Life Cycle")
+print("This is the repeatable steps of software development")
+print("")
+print("Step 1: Requirement analysis")
+print("    Understand what the program needs to do")
+print("Step 2: Planning/Design")
+print("    Map out the algorithm (pseudocode, flowcharts)")
+print("Step 3: Implementation")
+print("    Write the code with comments")
+print("Step 4: Testing")
+print("    Check for errors & correct results")
+print("Step 5: Release and Maintenance")
+print("    Share it - and keep fixing/updating it")
+print("")
+print("Algorithm: a step-by-step set of instructions for solving a problem, written in a way a computer (or a person) can follow exactly")
+print("Software Development Life Cycle (SDLC): the repeatable set of stages a program goes through, from idea to finished, maintained product")
+print("Pseudocode: a way of planning an algorithm using plain, structured language instead of actual code syntax — it lets you plan the logic before worrying about Python-specific rules")
+print("")
+print("Why does reliable software matter")
+print("    Unreliable software can cost peopl money, time, or even safety.")
+print("    A bug can cost real money")
+print("Requirement analysis")
+print("    3 questions:")
+print("     what is the problem?")
+print("     What inputs does it need?")
+print("     What outputs shuld it produce?")
+print("YOU CANNOT BUILD A PROGRAM IF THIS STEP IS SKIPPED")
+print("int mean just a number")
+print(5/2)
+print(5//2)
+print(" first number was plain number")
+print(" the second number was the decimal numbers dropped")
+print("modulo-> % ")
+print(5%2)
+print(10%4)
+print(15%5)
+print("returns the remainder of 5/2 10/4 15/5 respectivly")
+print("trying math question to computer")
+print("(2-1)*3+4%3")
+print("*<- means multiplaction")
+print((2-1)*3+4%3)
+print("|^ correct btw")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")
+print("")

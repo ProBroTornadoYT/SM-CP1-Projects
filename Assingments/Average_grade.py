38dff057324668524fb8346cb4146e02c8a138d3
ProBroTornadoYT <shreenayan.mehta@ucas-edu.net>
2026-09-02 08:55:29 -0600

    Average grade assingment

--- Assingments/Average_grade.py
+++ Assingments/Average_grade.py
@@ -2,20 +2,20 @@
 
 print("Hello user! Today we will calculate your grades average just put your grades in the feilds below!") #asking for grades
 
-grade1 = int(input("Grade1:"))
-grade2 = int(input("Grade2:"))
-grade3 = int(input("Grade3:"))
-grade4 = int(input("Grade4:"))
-grade5 = int(input("Grade5:"))
-grade6 = int(input("Grade6:"))
-grade7 = int(input("Grade7:"))
+grade1 = float(input("Grade1:"))
+grade2 = float(input("Grade2:"))
+grade3 = float(input("Grade3:"))
+grade4 = float(input("Grade4:"))
+grade5 = float(input("Grade5:"))
+grade6 = float(input("Grade6:"))
+grade7 = float(input("Grade7:"))
 
-print("") #<- for a little white space in program
-print("") #<- for a little white space in program
-print("") #<- for a little white space in program
+print("\n") #<- for a little white space in program
+print("\n") #<- for a little white space in program
+print("\n") #<- for a little white space in program
 
-print("calculating total grade")
+print("calculating Avg. grade")
 
-total_grade= int(print(grade7+grade6+grade5+grade4+grade3+grade2+grade1))
+total_grade=grade7+grade6+grade5+grade4+grade3+grade2+grade1
 
-print((total_grade)/2)
+print(round((total_grade)/7,2))

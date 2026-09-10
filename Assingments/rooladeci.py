7eeb0f39dcad020ed1209519c7efc92b952907b7
ProBroTornadoYT <shreenayan.mehta@ucas-edu.net>
2026-09-10 09:11:31 -0600

    rooladeci dnoe

--- Assingments/rooladeci.py
+++ Assingments/rooladeci.py
@@ -5,38 +5,32 @@ print("WELCOME TO THE DICE ROLLER PLEASE TYPE YOUR DIE TYPE BELOW!!")
 user_input = input('What size dice would you like to roll? (D4, D6, D8, D10, D12, D20,D100000:')
 
 if user_input == 'D4':
-    d4= random.int(1,4)
-else:
-    print("Wait!")
+    d4= random.randint(1,4)
+    print(d4)
+
 
 if user_input == 'D6':
-    d4= random.int(1,6)
-else:
-    print("Wait!")
+    d6= random.randint(1,6)
+    print(d6)
+
 
 if user_input == 'D8':
-    d4= random.int(1,8)
-else:
-    print("Wait!")
+    d8= random.randint(1,8)
+    print(d8)
+
 
 if user_input == 'D12':
-    d4= random.int(1,12)
-else:
-    print("Wait!")
+    d12= random.randint(1,12)
+    print(d12)
+
 
 if user_input == 'D20':
-    d4= random.int(1,20)
-else:
-    print("Wait!")
+    d20= random.randint(1,20)
+    print(d20)
+
+
 
 if user_input == 'D100000':
-    d4= random.int(1,100000)
-else:
-    print("Wait!")
+    d100000= random.randint(1,100000)
+    print(d100000)
 
-# secret easter egg :)
-
-if user_input == 'D67':
-    d4= random.int(1,67)
-else:
-    print("Wait! Input a valid die number")

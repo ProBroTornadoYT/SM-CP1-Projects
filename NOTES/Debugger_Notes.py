aaaaa7d4ec0dca1d3b7180f828398f0df99eeaa0
ProBroTornadoYT <shreenayan.mehta@ucas-edu.net>
2026-09-16 09:10:03 -0600

    fgh

---
+++ NOTES/Debugger_Notes.py
@@ -0,0 +1,14 @@
+# SM DEBUGGER NOTES
+
+grades = [85,90,78,92,88]
+
+total = 0
+count = 4
+
+for grade in grades:
+    total = total + grade
+
+average = total/count
+
+print(f"The avg. grade is {average}")
+
